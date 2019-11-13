e068b89e7f9404c3190417edaee226767df82c7b
liatmoss <liatmoss@hotmail.com>
2019-11-13 14:44:41 +0000

    set temperature

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,20 @@
+describe ('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  describe('temperature', function() {
+
+    it('starts at 20 degrees', function() {
+      expect(thermostat.temp).toEqual(20);
+    });
+
+  });
+
+
+
+
+});
