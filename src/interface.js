bf7128a9dcd14e2b6fa4ea6d103ff73db6ab6a44
Topher <topherb99@gmail.com>
2019-11-14 15:16:25 +0000

    linked jquery and css to dynamically change temperature colour readout

--- src/interface.js
+++ src/interface.js
@@ -12,7 +12,31 @@ $(document).ready(function() {
     updateTemp();
   })
 
+  $('#reset').click(function() {
+    thermostat.reset();
+    updateTemp();
+  })
+
+  $('#powersaving-on').click(function(){
+    thermostat.powerSave("on");
+    updatePSM();
+  })
+
+  $('#powersaving-off').click(function(){
+    thermostat.powerSave("off");
+    updatePSM();
+  })
+
+  function updatePSM() {
+    if (thermostat.isOn === true) {
+      $('#power-save').text("on")
+    } else {
+      $('#power-save').text("off")
+    }
+  }
+
   function updateTemp() {
     $('#temperature').text(thermostat.temp);
+    $('#temperature').attr('class', thermostat.currentEnergyUsage());
   }
 })
